58f8f815a6eae7fc0311019d5f7d92cb11a90903
bryllemutia <bryllemutia3@gmail.com>
2023-10-20 14:13:28 +0800

    fix: add account button flex basis

--- src/pages/Homepage/components/AddAccount.tsx
+++ src/pages/Homepage/components/AddAccount.tsx
@@ -9,7 +9,7 @@ const AddAccount = ({ text }: AddAccountProps) => {
   return (
     <button
       className="flex items-center rounded-md border-2 border-blue-accent px-1 py-1 text-[0.75rem] font-bold text-blue-accent"
-      style={{ flexBasis: "calc(33.333333% - 0.4em)" }}
+      style={{ flexBasis: "calc(33.333333% - 0.55em)" }}
     >
       <span className="text-left leading-4">{text}</span>{" "}
       <IoMdAddCircle style={{ width: "2em", height: "2em" }} />
