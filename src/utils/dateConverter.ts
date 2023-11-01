a49ee1047e794eb8a7137fad406462e2bee88dc6
bryllemutia <bryllemutia3@gmail.com>
2023-11-01 23:04:05 +0800

    util: more date convert functions

--- src/utils/dateConverter.ts
+++ src/utils/dateConverter.ts
@@ -1,3 +1,21 @@
-const dateConverter = (dateInSeconds: number): string => String(new Date(dateInSeconds * 1000))
+const dateConverter = (dateInSeconds: number): string =>
+  String(new Date(dateInSeconds * 1000));
 
+const dateConvertMonthYear = (dateConvert: string) => {
+  const newDate = new Date(dateConvert);
+  const datemmyyyy = `${newDate.getMonth()}/${newDate.getFullYear()}`;
+
+  return datemmyyyy;
+};
+
+const currentDateMonthYear = (monthModifier: number = 0) => {
+   const newDate = new Date();
+   const datemmyyyy = `${
+      newDate.getMonth() - monthModifier
+   }/${newDate.getFullYear()}`;
+   
+  return datemmyyyy;
+};
+
+export { dateConvertMonthYear, currentDateMonthYear };
 export default dateConverter;
