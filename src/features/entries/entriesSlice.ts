6bb074d3fc879de05eff0212623e39901ddee6e0
bryllemutia <bryllemutia3@gmail.com>
2023-11-14 16:31:41 +0800

    refactor: added docs to get totals function

--- src/features/entries/entriesSlice.ts
+++ src/features/entries/entriesSlice.ts
@@ -161,6 +161,16 @@ const getTotals = (
   mode: "expense" | "income" | "cashflow",
   monthModifier?: number
 ) =>
+  /**
+   * Function used to get total expense, income, and cashflow in specified month with relation to current month (month modifier)
+   * 
+   * @param   entriesPayload    all entries from state
+   * @param   mode              total to be calculated
+   * @param   monthModifier     how many months back to get with relation to current month (ex. 1 would be current month - 1 month)
+   * 
+   * @return  total
+   */
+
   entriesPayload.reduce((sum, current) => {
     if (
       dateConvertMonthYear(current.dateCreated) ===
