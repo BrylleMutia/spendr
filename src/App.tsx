8ec7da6cba9df8d6c0cd487411226c123c260aa7
bryllemutia <bryllemutia3@gmail.com>
2023-11-16 16:22:56 +0800

    feature: save and update current auth user

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,47 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 
 import "./App.css";
 import HomepageContainer from "./pages/Homepage/HomepageContainer";
 import Register from "./pages/Auth/Register";
+import Login from "./pages/Auth/Login";
+import PasswordReset from "./pages/Auth/PasswordReset";
+import { onAuthStateChanged } from "firebase/auth";
+import { firebaseAuth } from "./api/fireStore";
+import { useAppDispatch } from "./app/hooks";
+import { saveUser } from "./features/users/usersSlice";
+import { User } from "./features/users/userTypes";
 
 function App() {
+  const dispatch = useAppDispatch();
+
+  // save the user token from firebase to our global state and update it every time something happens to the user
+  useEffect(() => {
+    onAuthStateChanged(firebaseAuth, (user) => {
+      if (user) {
+        const updatedUserInfo: User = {
+          id: user.uid,
+          name: user.displayName,
+          email: user.email,
+          emailVerified: user.emailVerified,
+          dateCreated: user.metadata.creationTime,
+          photoURL: user.photoURL,
+          accessToken: user.refreshToken,
+        };
+
+        dispatch(saveUser(updatedUserInfo));
+      }
+    });
+  }, [firebaseAuth, dispatch]);
+
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<HomepageContainer />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/reset" element={<PasswordReset />} />
       </Route>
     </Routes>
   );
