376787c8721348b20e0ee1cf51047da269484745
bryllemutia <bryllemutia3@gmail.com>
2023-11-16 16:22:07 +0800

    feature: auth logout user

--- src/components/LinksCreator.tsx
+++ src/components/LinksCreator.tsx
@@ -1,16 +1,29 @@
+import { signOut } from "firebase/auth";
 import React from "react";
 import { AiFillHome } from "react-icons/ai";
 import { CgProfile } from "react-icons/cg";
 import { SlList } from "react-icons/sl";
 import { Link } from "react-router-dom";
+import { firebaseAuth } from "../api/fireStore";
 
 type LinkType = {
   text: string;
-  link: string;
   icon: React.ReactNode;
+  link?: string;
+  hasFunction?: () => void;
 };
 
 export const LinksCreator = () => {
+  const handleLogOut = () => {
+    signOut(firebaseAuth)
+      .then(() => {
+        console.log("user signed out");
+      })
+      .catch((error) => {
+        console.log("error", error);
+      });
+  };
+
   const links: LinkType[] = [
     {
       text: "Home",
@@ -27,12 +40,22 @@ export const LinksCreator = () => {
       link: "/records",
       icon: <SlList />,
     },
+    {
+      text: "Logout",
+      icon: <SlList />,
+      hasFunction: handleLogOut,
+    },
   ];
 
   return (
     <>
       {links.map((link, index) => (
-        <Link key={index} to={link.link} className="px-7 py-4 flex items-center gap-5 hover:bg-gray-text-1">
+        <Link
+          key={index}
+          to={link.link ? link.link : ""}
+          onClick={handleLogOut}
+          className="flex items-center gap-5 px-7 py-4 hover:bg-gray-text-1"
+        >
           {link.icon}
           {link.text}
         </Link>
