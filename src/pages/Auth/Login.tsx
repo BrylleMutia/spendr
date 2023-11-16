074a20f7f439454631e5979d0b03975bf24840ef
bryllemutia <bryllemutia3@gmail.com>
2023-11-16 16:21:20 +0800

    feature: auth login user

--- src/pages/Auth/Login.tsx
+++ src/pages/Auth/Login.tsx
@@ -1,7 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
+import { firebaseAuth } from "../../api/fireStore";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { useAppDispatch } from "../../app/hooks";
+import { Link } from "react-router-dom";
 
 const Login = () => {
-  return <div>Login</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const dispatch = useAppDispatch();
+
+  const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+  };
+  const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+  };
+
+  const handleUserLogin = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+
+    signInWithEmailAndPassword(firebaseAuth, email, password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        console.log("Singed in user: ", user);
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log("An error occured: ", errorCode, errorMessage);
+      });
+  };
+
+  // TODO: Design auth pages
+  return (
+    <div>
+      <h1>Login</h1>
+      <form method="POST">
+        <input
+          type="email"
+          id="email"
+          value={email}
+          onChange={handleEmailChange}
+          placeholder="Email"
+        />
+        <label htmlFor="email">Email</label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={handlePasswordChange}
+          placeholder="Password"
+        />
+        <label htmlFor="password">Password</label>
+        <button type="submit" onClick={handleUserLogin}>
+          Login
+        </button>
+        <Link to="/reset">Forgot Password</Link>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
