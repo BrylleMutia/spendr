c0e49774ede948cc294c172c0710f56dc4c6d099
bryllemutia <bryllemutia3@gmail.com>
2023-12-10 11:36:37 +0800

    feature: display photo and name on sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,5 +1,6 @@
 import { MdClose } from "react-icons/md";
 import { LinksCreator } from "./LinksCreator";
+import { useAppSelector } from "../app/hooks";
 
 type SidebarProps = {
   isOpen: boolean;
@@ -7,21 +8,27 @@ type SidebarProps = {
 };
 
 const Sidebar = ({ isOpen, toggleSidebar }: SidebarProps) => {
+  const { photoURL, name } = useAppSelector((state) => state.users.user);
+
   return (
     <aside>
       <div
-        className={`fixed left-[-520px] top-0 z-40 h-full w-[70vw] bg-white text-white  duration-300 ease-in-out shadow-md ${
+        className={`fixed left-[-520px] top-0 z-40 h-full w-[70vw] bg-white text-white  shadow-md duration-300 ease-in-out ${
           isOpen ? "translate-x-[520px] " : "translate-x-full"
         }`}
       >
-        <div className="-mt-2 flex gap-3 bg-blue-primary px-6 py-9">
+        <div className="-mt-2 flex items-center gap-3 bg-blue-primary px-6 py-9">
           <img
-            src="https://i.ibb.co/Ny0d4Ds/58874210.jpg"
+            src={
+              photoURL
+                ? photoURL
+                : "https://i.ibb.co/YRsRb90/handy-game-icon-1.png"
+            }
             alt="profile"
-            className="w-[50px] rounded-full"
+            className="h-[50px] w-[50px] rounded-full border-[1px] border-blue-accent bg-white p-[1px]"
           />
           <div className="flex flex-col">
-            <h3 className="text-lg font-semibold">Brylle Mutia</h3>
+            <h3 className="text-lg font-semibold">{name}</h3>
             <p className="text-sm leading-none">My Wallet</p>
           </div>
           <button onClick={toggleSidebar} className="ml-3">
@@ -29,7 +36,7 @@ const Sidebar = ({ isOpen, toggleSidebar }: SidebarProps) => {
           </button>
         </div>
         <nav>
-          <ul className="flex flex-col mt-5 text-gray-600">
+          <ul className="mt-5 flex flex-col text-gray-600">
             <LinksCreator />
           </ul>
         </nav>
