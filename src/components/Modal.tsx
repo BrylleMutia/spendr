6bb3c50b4ef4246d0f11d5998d0ec26619f01d2b
bryllemutia <bryllemutia3@gmail.com>
2024-01-14 20:01:58 +0800

    feature: close modal when clicked outside

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { createPortal } from "react-dom";
 import { MdClose } from "react-icons/md";
+import { useOutsideClick } from "./useOutsideClick";
 
 type ModalProps = {
   isOpen: boolean;
-  closeModal: (e: React.MouseEvent<HTMLButtonElement>) => void;
+  closeModal: () => void;
   children: React.ReactNode;
   hideCloseButton?: boolean;
 };
@@ -15,22 +16,29 @@ const Modal = ({
   children,
   hideCloseButton = false,
 }: ModalProps) => {
+  const clickedOutsideRef = useOutsideClick(() => closeModal());
+
+  const handleCloseModal = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.stopPropagation();
+    closeModal();
+  };
+
   if (!isOpen) return null;
 
   return createPortal(
-    <>
+    <div ref={clickedOutsideRef}>
       <div className="fixed bottom-0 left-0 right-0 top-0 z-50 bg-black opacity-[0.7]"></div>
       <div className="fixed left-1/2 top-1/2 z-50 -translate-x-1/2 -translate-y-1/2 rounded-lg bg-white">
         <button
           hidden={hideCloseButton}
-          onClick={(e) => closeModal(e)}
+          onClick={(e) => handleCloseModal(e)}
           className="absolute right-4 top-4"
         >
           <MdClose />
         </button>
         {children}
       </div>
-    </>,
+    </div>,
     document.getElementById("modal")!,
   );
 };
